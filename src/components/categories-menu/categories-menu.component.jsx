9814f3546a489e5dbdd476402215e1045def9ddf
Zenith Hanif <zenithpkhanif@gmail.com>
2024-01-03 15:46:17 +0500

    Updating home page with categories menu component and categories item component

---
+++ src/components/categories-menu/categories-menu.component.jsx
@@ -0,0 +1,18 @@
+import './categories-menu.styles.scss'
+
+import CategoryItem from '../category-item/category-item.components'
+
+const CategoryMenu = ({categories}) => {
+    
+      return (
+        <div className="categories-container">
+        {
+            categories.map((category) => (
+            <CategoryItem key={category.id} category={category}/>
+            ))
+        }      
+        </div>
+      )
+}
+
+export default CategoryMenu;
